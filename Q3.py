88cd58e544d8c1ed3c6f682d414a08cd335b9586
Jeremy Varghese <jeremy.varghese11@gmail.com>
2023-10-09 00:33:45 -0400

    Minimax

--- Q3.py
+++ Q3.py
@@ -7,11 +7,11 @@ Created on Fri Oct  6 15:09:56 2023
 
 class BinaryTree():
 
-    def __init__(self,rootid):
+    def __init__(self,rootid,value):
       self.left = None
       self.right = None
       self.rootid = rootid
-      self.value = 0
+      self.value = value
 
     def getLeftChild(self):
         return self.left
@@ -24,19 +24,19 @@ class BinaryTree():
     def getNodeId(self):
         return self.rootid
 
-    def insertRight(self,newNode):
+    def insertRight(self,newNode,value):
         if self.right == None:
-            self.right = BinaryTree(newNode)
+            self.right = BinaryTree(newNode,value)
         else:
-            tree = BinaryTree(newNode)
+            tree = BinaryTree(newNode,value)
             tree.right = self.right
             self.right = tree
 
-    def insertLeft(self,newNode):
+    def insertLeft(self,newNode,value):
         if self.left == None:
-            self.left = BinaryTree(newNode)
+            self.left = BinaryTree(newNode,value)
         else:
-            tree = BinaryTree(newNode)
+            tree = BinaryTree(newNode,value)
             tree.left = self.left
             self.left = tree
 
@@ -44,20 +44,113 @@ class BinaryTree():
 def printTree(tree):
         if tree != None:
             printTree(tree.getLeftChild())
-            print(tree.getNodeId())
+            print(tree.getNodeValue())
             printTree(tree.getRightChild())
 
 def testTree():
-    myTree = BinaryTree("l_0")
-    myTree.insertLeft("l_10")
-    myTree.insertRight("l_11")
+    myTree = BinaryTree("l_0",0)
+    myTree.insertLeft("l_10",1)
+    myTree.insertRight("l_11",2)
     l10 = myTree.getLeftChild()
-    l10.insertLeft("l_20")
-    l10.insertRight("l_21")
+    l10.insertLeft("l_20",3)
+    l10.insertRight("l_21",4)
     l11 = myTree.getRightChild()
-    l11.insertLeft("l_22")
-    l11.insertRight("l_23")
+    l11.insertLeft("l_22",5)
+    l11.insertRight("l_23",6)
 
     printTree(myTree)
 
 #testTree()
+
+def value(node,state):
+    if(node.getLeftChild()==None and node.getRightChild()==None):
+        return node.getNodeValue()
+    elif(state=="Min"):                                                         #if curr state is Min, then next states are max
+        node.setNodeValue(float('inf'))
+        return min(node.getNodeValue(),max_value(node.getLeftChild()),max_value(node.getRightChild()))
+    elif(state=="Max"):
+        node.setNodeValue=float("-inf")
+        return max(node.getNodeValue(),min_value(node.getLeftChild()),min_value(node.getRightChild()))
+        
+def min_value(node):
+    if(node==None):
+        return float('-inf')
+    #node.value = float("inf") 
+    if(node.getLeftChild()==None and node.getRightChild()==None):
+         print("min val for ",node.getNodeId()," is ", node.getNodeValue())
+         return node.getNodeValue()
+    node.setNodeValue(float("inf"))
+    val = min(node.getNodeValue(),max_value(node.getLeftChild()),max_value(node.getRightChild()))
+    print("min val for ",node.getNodeId()," is ", val)
+    node.setNodeValue = val
+    return val
+    
+def max_value(node):
+    if(node==None):
+        return float("inf")
+    #node.value = float("-inf")
+    if(node.getLeftChild()==None and node.getRightChild()==None):
+        print("max val for ",node.getNodeId()," is ", node.getNodeValue())
+        return node.getNodeValue()
+    node.setNodeValue(float("-inf"))
+    val =  max(node.getNodeValue(),min_value(node.getLeftChild()),min_value(node.getRightChild()))
+    print("max val for ",node.getNodeId()," is ", val)
+    node.setNodeValue(val)
+    return val
+
+tree = BinaryTree("Root",0)
+tree.insertLeft("H_1_L",0)
+tree.insertRight("H_1_R",0)
+
+l1 = tree.getLeftChild()
+r1 = tree.getRightChild()
+
+l1.insertLeft("H_2_L_L",0)
+l1.insertRight("H_2_L_R",0)
+r1.insertLeft("H_2_R_L",0)
+r1.insertRight("H_2_R_R",0)
+
+l2l = l1.getLeftChild()
+l2r = l1.getRightChild()
+r2l = r1.getLeftChild()
+r2r = r1.getRightChild()
+
+l2l.insertLeft("H_3_L_L_L",0)
+l2l.insertRight("H_3_L_L_R",0)
+l2r.insertLeft("H_3_L_R_L",0)
+l2r.insertRight("H_3_L_R_R",0)
+r2l.insertLeft("H_3_R_L_L", 0)
+r2l.insertRight("H_3_R_L_R",0)
+r2r.insertLeft("H_3_R_R_L",0)
+r2r.insertRight("H_3_R_R_R",0)
+
+HLLL = l2l.getLeftChild()
+HLLR = l2l.getRightChild()
+HLRL = l2r.getLeftChild()
+HLRR = l2r.getRightChild()
+HRLL = r2l.getLeftChild()
+HRLR = r2l.getRightChild()
+HRRL = r2r.getLeftChild()
+HRRR = r2r.getRightChild()
+
+HLLL.insertLeft("Leaf_1",3)
+HLLL.insertRight("Leaf_2",10)
+HLLR.insertLeft("Leaf_3",2)
+HLLR.insertRight("Leaf_4",9)
+HLRL.insertLeft("Leaf_5",10)
+HLRL.insertRight("Leaf_6",7)
+HLRR.insertLeft("Leaf_7",5)
+HLRR.insertRight("Leaf_8",9)
+HRLL.insertLeft("Leaf_9",2)
+HRLL.insertRight("Leaf_10",5)
+HRLR.insertLeft("Leaf_11",6)
+HRLR.insertRight("Leaf_12",4)
+HRRL.insertLeft("Leaf_13",2)
+HRRL.insertRight("Leaf_14",7)
+HRRR.insertLeft("Leaf_15",9)
+HRRR.insertRight("Leaf_16",1)
+
+#printTree(tree)
+
+output = value(tree,"Max")
+print(output)
